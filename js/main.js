48ba544acf0047c4c260d6f632db931e700c7bd4
Kalwarpriyanka <100395709+Kalwarpriyanka@users.noreply.github.com>
2022-04-09 15:09:46 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,71 @@
+// $(document).ready(function(){
+
+//     $(".filter-button").click(function(){
+//         var value = $(this).attr('data-filter');
+        
+//         if(value == "all")
+//         {
+//             //$('.filter').removeClass('hidden');
+//             $('.filter').show('1000');
+//         }
+//         else
+//         {
+// //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+// //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+//             $(".filter").not('.'+value).hide('3000');
+//             $('.filter').filter('.'+value).show('3000');
+            
+//         }
+//     });
+    
+//     if ($(".filter-button").removeClass("active")) {
+// $(this).removeClass("active");
+//     }
+// $(this).addClass("active");{
+//     $("#exampleModal").modal('show');
+
+
+// });
+
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+
+
+
+
+$(window).scroll(function(){
+    if($(this).scrollTop()> 250)
+    {
+        $(".arrow").show()
+    }
+    else
+    {
+        $(".arrow").hide()    
+    }
+});
